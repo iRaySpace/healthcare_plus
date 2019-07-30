2c6494d9be3a5954d1b2b10491addb4119a66ec9
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-30 09:45:49 +0800

    feat: healthcare plus settings

---
+++ healthcare_plus/healthcare_plus/doctype/healthcare_plus_settings/healthcare_plus_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Ivan Ray Altomera and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Healthcare Plus Settings', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ healthcare_plus/healthcare_plus/doctype/healthcare_plus_settings/test_healthcare_plus_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Ivan Ray Altomera and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestHealthcarePlusSettings(unittest.TestCase):
+	pass

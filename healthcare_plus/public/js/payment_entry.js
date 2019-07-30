481e13ccbdda394b0aeb22a1be1c58ea24e5706b
Shiela <shiela@bai.ph>
2019-07-30 11:32:52 +0800

    fix: remove console.log()

--- healthcare_plus/public/js/payment_entry.js
+++ healthcare_plus/public/js/payment_entry.js
@@ -4,7 +4,6 @@
 cur_frm.cscript.hp_insurance_coverage= function (frm) {
 	if (cur_frm.doc.hp_insurance_coverage==1){
 		cur_frm.set_df_property("hp_insurance_provider", "reqd", 1);
-		console.log("test")
 	}else{
 		cur_frm.set_df_property("hp_insurance_provider", "reqd", 0);
 	}

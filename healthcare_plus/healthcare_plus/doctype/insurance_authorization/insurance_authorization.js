729e569e1a76d864aa7c6fcf63ff9232d17d74ff
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-31 14:08:02 +0800

    feat: added insurance authorization

---
+++ healthcare_plus/healthcare_plus/doctype/insurance_authorization/insurance_authorization.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Ivan Ray Altomera and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Insurance Authorization', {
+	refresh: function(frm) {
+
+	}
+});

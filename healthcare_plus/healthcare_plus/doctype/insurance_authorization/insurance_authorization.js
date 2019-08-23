cf4f6e94e5092f2addf97bf0d8c73bae2f7a9d86
Shiela <shiela@bai.ph>
2019-08-23 14:47:25 +0800

    feat: update insurance authorization properties

--- healthcare_plus/healthcare_plus/doctype/insurance_authorization/insurance_authorization.js
+++ healthcare_plus/healthcare_plus/doctype/insurance_authorization/insurance_authorization.js
@@ -13,9 +13,10 @@ frappe.ui.form.on('Insurance Authorization', {
                     name: cur_frm.doc.patient_encounter
                 },
                 callback: function (r) {
-                    var fields = ['patient', 'message', 'authorized_by', 'code', 'description', 'medical_code', 'visit_date']
+                    var fields = ['patient', 'message', 'authorized_by', 'code', 'description', 'medical_code', 'visit_date', 'visit_number']
                     var PE = r.message[0]
                     var medical_code = r.message[1]
+                    cur_frm.doc.visit_number = cur_frm.doc.patient_encounter
                     cur_frm.doc.patient = PE[1]
                     cur_frm.doc.visit_date = r.message[2]
                     cur_frm.doc.message = PE[5]
@@ -28,6 +29,12 @@ frappe.ui.form.on('Insurance Authorization', {
                     }
                 }
             })
+        }else{
+		    var f = ['patient', 'message', 'authorized_by', 'code', 'description', 'medical_code', 'visit_date', 'visit_number']
+            for (var i = 0; i < f.length; i++) {
+		        cur_frm.doc[f[i]]=""
+                refresh_field(f[i])
+            }
         }
     },
 	medical_code: function (frm) {
@@ -35,14 +42,16 @@ frappe.ui.form.on('Insurance Authorization', {
 			method: "frappe.client.get_value",
 			args: {
 				doctype: "Medical Code",
-				fieldname: ['code'],
+				fieldname: ['code', 'description'],
 				filters: {
 					name: cur_frm.doc.medical_code
 				}
 			},
 			callback: function (r) {
 				cur_frm.doc.code = r.message['code']
+				cur_frm.doc.description = r.message['description']
 				refresh_field('code')
+				refresh_field('description')
             }
 		})
     }
